ca95cfcff98b3455246b7e09576cdddc62eb1a01
Kovekk <fishm3001@gmail.com>
2022-03-07 19:42:52 -0700

    changing from snakes to bikes

--- Bike/__main__.py
+++ Bike/__main__.py
@@ -1,16 +1,14 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
-from game.casting.score import Score
-from game.casting.snake import Snake
+from game.casting.bike import Bike
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
 from game.scripting.handle_collisions_action import HandleCollisionsAction
 from game.scripting.draw_actors_action import DrawActorsAction
 from game.directing.director import Director
-from game.services.keyboard_service import KeyboardService
+from game.services.keyboard_services import KeyboardService
 from game.services.video_service import VideoService
 from game.shared.color import Color
 from game.shared.point import Point
@@ -20,9 +18,8 @@ def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Snake())
-    cast.add_actor("scores", Score())
+    cast.add_actor("bikes", Bike())
+    cast.add_actor("bikes", Bike())
    
     # start the game
     keyboard_service = KeyboardService()
